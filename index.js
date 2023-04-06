56f49d5cd227f9d1a011914eeebdf2fd38bf5775
Camilo Castro <castroh24@gmail.com>
2023-04-06 10:22:05 -0600

    Employee, Director and Manager classes added. Bonus applied based on current roll

--- index.js
+++ index.js
@@ -4,32 +4,17 @@ import { Cuenta } from './Cuentas/Cuenta.js';
 import {CuentaAhorro} from './Cuentas/CuentaAhorro.js';
 import { CuentaCorriente } from './Cuentas/CuentaCorriente.js';
 import { CuentaNomina } from './Cuentas/CuentaNomina.js';
+import { Director } from './Cuentas/Empleados/Director.js';
+import { Empleado } from './Cuentas/Empleados/Empleado.js';
+import { Gerente } from './Cuentas/Empleados/Gerente.js';
 
 const cliente = new Cliente('Leonardo','13804050','123224');
 const cliente2 = new Cliente('María','16979808','8989');
 
-const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
-const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
+const empleado = new Empleado('Juan Perez', '11223366',10000);
+const gerente =  new Gerente('Pedro Rivas','2366113',12000 );
+const director = new Director('Elena MOreno', '1123351',15000);
 
-const cuentaAhorroLeo = new CuentaAhorro (cliente, '9985','001',0);
-
-
-console.log(cuentaDeLeonardo);
-console.log("Se depositan 150");
-cuentaDeLeonardo.depositoEnCuenta(150);
-console.log(cuentaDeLeonardo.verSaldo());
-console.log("Se retiran 70");
-cuentaDeLeonardo.retirarDeCuenta(70);
-console.log(cuentaDeLeonardo.verSaldo());
-console.log(cuentaAhorroLeo);
-console.log("Se depositan:" + cuentaAhorroLeo.depositoEnCuenta(200));
-cuentaAhorroLeo.retirarDeCuenta(120);
-//console.log(cuentaAhorroLeo.retirarDeCuenta(80));
-console.log(cuentaAhorroLeo.verSaldo());
-
-
-const cuentaNominaAna = new CuentaNomina(cliente, '0098', '002', 100);
-cuentaNominaAna.depositoEnCuenta(150);
-cuentaNominaAna.retirarDeCuenta(50);
-console.log(cuentaNominaAna.verSaldo());
-
+console.log(empleado.verBonificacion());
+console.log(gerente.verBonificacion());
+console.log(director.verBonificacion());
