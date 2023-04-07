aa3bcb9cae093b4255f0fc1337b19f3b86c31bd5
Camilo Castro <castroh24@gmail.com>
2023-04-06 20:45:42 -0600

    object type and atributes validated with a function from SisitemaAutenticacion class

--- Empleados/Empleado.js
+++ Empleados/Empleado.js
@@ -1,13 +1,24 @@
+
 export class Empleado{
     #nombre;
     #dni;
     #salario;
+    #clave;
 
     constructor(nombre, dni, salario){
         this.#nombre = nombre;
         this.#dni = dni;
         this.#salario = salario;
+        this.#clave = '';
     }
+    asignarClave(clave){
+        this.#clave = clave;
+    }
+
+    autenticable(clave){
+        return clave == this.#clave;
+    }
+
     verBonificacion(){
         return this.#salario;
     }
--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 /*Importación de clases*/
 import {Cliente} from './Cliente.js'
-import { Cuenta } from './Cuentas/Cuenta.js';
-import {CuentaAhorro} from './Cuentas/CuentaAhorro.js';
-import { CuentaCorriente } from './Cuentas/CuentaCorriente.js';
-import { CuentaNomina } from './Cuentas/CuentaNomina.js';
-import { Director } from './Cuentas/Empleados/Director.js';
-import { Empleado } from './Cuentas/Empleados/Empleado.js';
-import { Gerente } from './Cuentas/Empleados/Gerente.js';
+import { SistemaAutenticacion } from './SistemaAutenticacion.js';
+import { Director } from './Empleados/Director.js';
+import { Empleado } from './Empleados/Empleado.js';
+import { Gerente } from './Empleados/Gerente.js';
 
-const cliente = new Cliente('Leonardo','13804050','123224');
-const cliente2 = new Cliente('María','16979808','8989');
+
+
+
+//const cliente2 = new Cliente('María','16979808','8989');
 
 const empleado = new Empleado('Juan Perez', '11223366',10000);
 const gerente =  new Gerente('Pedro Rivas','2366113',12000 );
-const director = new Director('Elena MOreno', '1123351',15000);
+//const director = new Director('Elena MOreno', '1123351',15000);
 
-console.log(empleado.verBonificacion());
-console.log(gerente.verBonificacion());
-console.log(director.verBonificacion());
+
+gerente.asignarClave('6556');
+
+empleado.asignarClave('12345');
+console.log(SistemaAutenticacion.login(gerente, '6556'));
+console.log(SistemaAutenticacion.login(empleado, '12345'));
+
+const cliente = new Cliente('Leonardo','13804050','123224');
+cliente.asignarClave('1111');
+console.log(SistemaAutenticacion.login(cliente, '1111'));
